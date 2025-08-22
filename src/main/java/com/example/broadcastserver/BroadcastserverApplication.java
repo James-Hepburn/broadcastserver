a2105762d33881111df5adfb14ac159c7565225f
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-22 14:53:46 -0600

    Initial commit

---
+++ src/main/java/com/example/broadcastserver/BroadcastserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.broadcastserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroadcastserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroadcastserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/broadcastserver/BroadcastserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.broadcastserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroadcastserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
