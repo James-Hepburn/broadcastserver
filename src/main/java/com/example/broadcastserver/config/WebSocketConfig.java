c5d304c86fba15e8790c4e3994215101ee19ede0
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-22 18:22:10 -0600

    WebSocketConfig

--- src/main/java/com/example/broadcastserver/config/WebSocketConfig.java
+++ src/main/java/com/example/broadcastserver/config/WebSocketConfig.java
@@ -1,4 +1,25 @@
 package com.example.broadcastserver.config;
 
-public class WebSocketConfig {
+import com.example.broadcastserver.server.BroadcastHandler;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+@AllArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private BroadcastHandler handler;
+
+    @Override
+    public void registerWebSocketHandlers (WebSocketHandlerRegistry registry) {
+        registry.addHandler (this.handler, "/chat")
+                .setAllowedOrigins ("*");
+    }
+
+    @Bean
+    public BroadcastHandler handler () {
+        return new BroadcastHandler ();
+    }
 }
